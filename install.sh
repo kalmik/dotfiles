7a6e411f6b81fefbfc5f48dae9cd050ca3e5864d
sergio.filipe@evolux.net.br <sergio.kalmik@gmail.com>
2017-12-29 15:16:41 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@ if [ "$(uname)" == "Darwin" ]; then
     brew install tmux
     brew install reattach-to-user-namespace
     brew install vim
+    ln -sfv "$DIR/.bash_profile" ~/.vim_profile
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo apt-get install tmux
     sudo apt-get install vim
     sudo apt-get install curl
+    ln -sfv "$DIR/.bashrc" ~/.bashrc
 fi
 
-mkdir NeoBundle
-curl -o NeoBundle/install.sh https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh
-sh ./NeoBundle/install.sh
+ln -sfv "$DIR/.vimrc" ~/.vimrc
+ln -sfv "$DIR/.tmux.conf" ~/.tmux.conf
+ln -sfv "$DIR/.gitconfig" ~/.gitconfig
+ln -sfv "$DIR/.gitignore" ~/.gitignore
+ln -sfv "$DIR/.gitignore_global" ~/.gitignore_global
